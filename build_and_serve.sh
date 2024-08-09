671e4f83c88390ec613c234a655e01c65837c1c8
Jorge V. Contreras <contreras.jorgev@gmail.com>
2024-08-09 10:32:53 -0500

    Old pagefind output was not being tracked as deleted.

--- build_and_serve.sh
+++ build_and_serve.sh
@@ -3,6 +3,9 @@
 # Build the Jekyll site
 bundle exec jekyll build
 
+# Remove the current pagefind folder (output of the pagefind command). This is necessary to have `git` track the deletions of old commits for this folder.
+rm -rf ./pagefind
+
 # Run the pagefind command
 pagefind --site ./_site --output-path ./pagefind
 
